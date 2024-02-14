e9c1108b6570bf0d87e3054fdcf521629294ed96
Alan Haber <dev@alanhaber.me>
2024-02-14 22:25:14 +0100

    feat: add Radio

---
+++ components/ui/Radio/component.tsx
@@ -0,0 +1,36 @@
+// region Imports
+import React from 'react';
+import { RadioProps } from './component.types';
+// endregion
+
+// region Component
+/**
+ * @name Radio
+ * @param {RadioProps} props The props of the component
+ * @returns {React.ReactElement} The React element
+ */
+const Radio: React.FC<RadioProps> = (props) => {
+  return (
+    <div className="flex items-center gap-2">
+      <input
+        type="radio"
+        id={props.id}
+        name={props.name}
+        disabled={props.disabled}
+        checked={props.checked === props.id}
+        onChange={props.onChange}
+        value={props.id}
+        className="peer relative h-4 w-4 cursor-pointer appearance-none rounded-full border border-gray-300 after:absolute after:left-[2px] after:top-[2px] after:h-[10px] after:w-[10px] after:rounded-full after:bg-black after:content-none checked:border-gray-500 checked:after:content-[''] disabled:cursor-default disabled:border-gray-300 disabled:after:bg-gray-400 dark:border-gray-100 after:dark:bg-gray-100 disabled:dark:border-gray-600 disabled:dark:after:bg-gray-600"
+      />
+      <label
+        htmlFor={props.id}
+        className={`text-sm font-500 peer-disabled:text-gray-500 peer-disabled:dark:text-gray-600`}
+      >
+        {props.label}
+      </label>
+    </div>
+  );
+};
+// endregion
+
+export default Radio;
---
+++ components/ui/Radio/component.types.ts
@@ -0,0 +1,28 @@
+type RadioProps = {
+  /**
+   * Radio value
+   */
+  checked: string;
+  /**
+   * Radio onChange event
+   */
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  /**
+   * Radio id
+   */
+  id: string;
+  /**
+   * Radio name
+   */
+  name: string;
+  /**
+   * Radio label
+   */
+  label: string;
+  /**
+   * Radio disabled
+   */
+  disabled?: boolean;
+};
+
+export { type RadioProps };
