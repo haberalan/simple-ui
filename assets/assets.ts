f722bddb95f9f2053d84ea86a05f2bc9bfbef94b
Alan Haber <dev@alanhaber.me>
2024-02-14 22:19:13 +0100

    feat: add assets.ts

---
+++ assets/assets.ts
@@ -0,0 +1,18 @@
+const ICONS = {
+  ArrowRight: 'ArrowRight',
+  ChevronDown: 'ChevronDown',
+  ChevronLeft: 'ChevronLeft',
+  ChevronRight: 'ChevronRight',
+  Check: 'Check',
+  Sunset: 'Sunset',
+  Eye: 'Eye',
+  EyeOff: 'EyeOff',
+  Search: 'Search',
+  Close: 'Close',
+  Layout: 'Layout',
+  Copy: 'Copy',
+  Moon: 'Moon',
+  Sun: 'Sun',
+};
+
+export { ICONS };
