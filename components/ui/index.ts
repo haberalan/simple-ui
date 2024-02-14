7f7b8bddbe501a1c7cbae54e35c4e014b2d45eba
Alan Haber <dev@alanhaber.me>
2024-02-14 22:19:49 +0100

    feat: add components index

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,3 +1,63 @@
-import ThemeProvider from "./ThemeProvider";
+import ThemeProvider, { ThemeContext } from './ThemeProvider';
+import Button from './Button';
+import Icon from './Icon';
+import { Accordion, AccordionItem } from './Accordion';
+import Link from './Link';
+import ActiveLink from './ActiveLink';
+import Loader from './Loader';
+import AlertProvider, { AlertContext } from './AlertProvider';
+import Card from './Card';
+import Separator from './Separator';
+import { Tabs, TabItem } from './Tabs';
+import Dialog from './Dialog';
+import Badge from './Badge';
+import Checkbox from './Checkbox';
+import Switch from './Switch';
+import Tooltip from './Tooltip';
+import Toggle from './Toggle';
+import Radio from './Radio';
+import Slider from './Slider';
+import Sheet from './Sheet';
+import Input from './Input';
+import Textarea from './Textarea';
+import TableOfContents from './TableOfContents';
+import Skeleton from './Skeleton';
+import Avatar from './Avatar';
+import Select from './Select';
+import Command from './Command';
+import IconButton from './IconButton';
 
-export { ThemeProvider };
+export {
+  ThemeProvider,
+  ThemeContext,
+  Button,
+  Icon,
+  Accordion,
+  AccordionItem,
+  Link,
+  ActiveLink,
+  Loader,
+  AlertProvider,
+  AlertContext,
+  Card,
+  Separator,
+  Tabs,
+  TabItem,
+  Dialog,
+  Badge,
+  Checkbox,
+  Switch,
+  Tooltip,
+  Toggle,
+  Radio,
+  Slider,
+  Sheet,
+  Input,
+  Textarea,
+  TableOfContents,
+  Skeleton,
+  Avatar,
+  Select,
+  Command,
+  IconButton,
+};
