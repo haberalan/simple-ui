1341c334ad75a651ac57b92686969c8b9f1af92b
Alan Haber <dev@alanhaber.me>
2024-02-21 19:01:10 +0100

    fix: icon

--- components/ui/Icon/component.tsx
+++ components/ui/Icon/component.tsx
@@ -1,8 +1,8 @@
 // region Imports
-import React from 'react';
-import { IconProps } from './component.types';
-import { ICONS } from '@/assets/assets';
-import { ICON_FILES } from './component.constants';
+import React from "react";
+import { IconProps } from "./component.types";
+import { ICONS } from "@/assets/assets";
+import { ICON_FILES } from "./component.constants";
 // endregion
 
 // region Component
@@ -12,11 +12,16 @@ import { ICON_FILES } from './component.constants';
  * @returns {React.ReactElement} The React element
  */
 const Icon: React.FC<IconProps> = (props) => {
-  const iconName = ICONS[props.name ?? 'ArrowRight'];
+  const iconName = ICONS[props.name ?? "ArrowRight"];
 
-  const SVGIcon: React.FC<React.SVGProps<SVGSVGElement>> | undefined = ICON_FILES[iconName as keyof typeof ICONS];
+  const SVGIcon: React.FC<React.SVGProps<SVGSVGElement>> | undefined =
+    ICON_FILES[iconName as keyof typeof ICONS];
 
-  return SVGIcon && <SVGIcon onClick={props.onClick} className={props.className ?? ''} />;
+  return (
+    SVGIcon && (
+      <SVGIcon onClick={props.onClick} className={props.className ?? ""} />
+    )
+  );
 };
 // endregion
 
