5273b1acbaa979d67987a669c1e940649b0807b3
Alan Haber <dev@alanhaber.me>
2024-02-21 19:00:24 +0100

    feat: add header

--- app/_components/Header/component.tsx
+++ app/_components/Header/component.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Link from "next/link";
+import ThemeMenu from "./ThemeMenu";
+import SearchMenu from "./SearchMenu";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="h-[58px]">
+      <div className="fixed top-0 z-10 w-full border-b border-gray-300 text-sm backdrop-blur-md dark:border-gray-900 dark:text-gray-400">
+        <div className="container flex items-center justify-between py-3">
+          <Link href="/" className="hover:underline">
+            <h1 className="text-md font-500 dark:text-white">simple-ui</h1>
+          </Link>
+          <div className="flex items-center justify-center gap-4">
+            <SearchMenu />
+            <ThemeMenu />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
