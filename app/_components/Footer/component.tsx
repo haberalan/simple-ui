33f0f0e74eb72292c3cc2b7341d75a1845fd9aff
Alan Haber <dev@alanhaber.me>
2024-02-21 19:00:17 +0100

    feat: add footer

--- app/_components/Footer/component.tsx
+++ app/_components/Footer/component.tsx
@@ -1,7 +1,44 @@
+"use client";
+
 import React from "react";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="border-t border-gray-300 text-sm dark:border-gray-900 dark:text-gray-400">
+      <div className="container flex items-center justify-between py-6">
+        <p>
+          Created by <span className="font-500">Alan Haber</span> &copy;{" "}
+          {new Date().getFullYear()}
+        </p>
+        <div className="flex items-center justify-center gap-2">
+          <a
+            href="https://github.com/"
+            target="_blank"
+            aria-label="Will open in new tab."
+            className="hover:underline"
+          >
+            Github
+          </a>
+          <a
+            href="https://www.youtube.com/@haberalan"
+            target="_blank"
+            aria-label="Will open in new tab."
+            className="hover:underline"
+          >
+            Youtube
+          </a>
+          <a
+            href="https://www.linkedin.com/in/alanhaber/"
+            target="_blank"
+            aria-label="Will open in new tab."
+            className="hover:underline"
+          >
+            Linkedin
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
