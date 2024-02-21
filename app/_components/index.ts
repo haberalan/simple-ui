fce5799493c5eb4a294f5afabd6bcd10acb3153a
Alan Haber <dev@alanhaber.me>
2024-02-21 19:00:52 +0100

    feat: add global components index

---
+++ app/_components/index.ts
@@ -0,0 +1,5 @@
+import Header from "./Header/component";
+import Navbar from "../(sites)/components/_components/Navbar/component";
+import Footer from "./Footer/component";
+
+export { Header, Navbar, Footer };
