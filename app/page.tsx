9fb52c90b0acb8b48bfe6e6f6b8606a8107897cf
Alan Haber <dev@alanhaber.me>
2024-02-22 22:34:04 +0100

    feat: temporary homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Hello, world!</h1>
+    <main className="container flex flex-1 flex-col items-center justify-center">
+      <section className="flex flex-col items-center gap-4">
+        <h1 className="text-display-2xl font-600">Hello World!</h1>
+        <p className="max-w-[800px] text-center text-md text-gray-600">
+          Crafted with TypeScript, Tailwind CSS, and Framer Motion, our library
+          offers a comprehensive suite of reusable components and animations.
+          Streamline development, enhance user experiences, and unleash
+          creativity with our flexible and responsive toolkit.
+        </p>
+      </section>
     </main>
   );
 }
