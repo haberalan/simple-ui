71ba93e596d4a62026d11dac291cd53a7f505bec
Alan Haber <dev@alanhaber.me>
2024-02-25 02:55:54 +0100

    feat: add metadata to pages

--- app/(sites)/components/[[...slug]]/page.tsx
+++ app/(sites)/components/[[...slug]]/page.tsx
@@ -1,6 +1,35 @@
 import React from "react";
 import { PREVIEWS } from "@/components/preview";
 import { redirect } from "next/navigation";
+import { COMPONENTS } from "@/constants";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug?: string[] };
+}) {
+  if (params.slug) {
+    const component = COMPONENTS.find(
+      (component) =>
+        component.name
+          .replace(/([a-z])([A-Z])/g, "$1-$2")
+          .replace(/^-/, "")
+          .toLocaleLowerCase() === params.slug?.[0].toLocaleLowerCase(),
+    );
+
+    if (component) {
+      return {
+        title: `${component.name} | simple-ui`,
+        description: component.description,
+      };
+    }
+  }
+
+  return {
+    title: "Components | simple-ui",
+    description: "It is a main components page.",
+  };
+}
 
 export default function Page({ params }: { params: { slug?: string[] } }) {
   if (params.slug) {
