116cec3e488f8cd910fc8f006469539f6df9287a
Alan Haber <dev@alanhaber.me>
2024-05-13 09:09:22 +0200

    refactor: radio pointers

--- components/ui/Radio/component.tsx
+++ components/ui/Radio/component.tsx
@@ -20,7 +20,7 @@ const Radio: React.FC<RadioProps> = (props) => {
         checked={props.checked === props.id}
         onChange={props.onChange}
         value={props.id}
-        className="peer relative size-4 cursor-pointer appearance-none rounded-full border border-gray-300 after:absolute after:left-[2px] after:top-[2px] after:size-[10px] after:rounded-full after:bg-black after:content-none checked:border-gray-500 checked:after:content-[''] disabled:cursor-default disabled:border-gray-300 disabled:after:bg-gray-400 dark:border-gray-100 after:dark:bg-gray-100 disabled:dark:border-gray-600 disabled:dark:after:bg-gray-600"
+        className="peer relative size-4 cursor-pointer appearance-none rounded-full border-2 border-gray-300 after:absolute after:left-[2px] after:top-[2px] after:size-[8px] after:rounded-full after:bg-black after:content-none checked:border-gray-500 checked:after:content-[''] disabled:cursor-default disabled:border-gray-300 disabled:after:bg-gray-400 dark:border-gray-100 after:dark:bg-gray-100 disabled:dark:border-gray-600 disabled:dark:after:bg-gray-600"
       />
       <label
         htmlFor={props.id}
